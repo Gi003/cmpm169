5d10508719d55bfcba8dd27a17405f637a5e53ee
gi003 <diraheta@ucsc.edu>
2025-01-20 23:06:53 -0800

    Added info

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -1,6 +1,6 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Gio
+// Date: Jan 15 20205
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -53,24 +53,20 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  background(255);
+  
+  for (let i=0; i < 800; i = i + 20){
+    for(let j=0; j < 800; j = j + 20){
+      dir_x = i - mouseX;
+      dir_y = j - mouseY;
+      magnitude = sqrt(sq(dir_x)+sq(dir_y));
+      u_x = 10 * (dir_x / magnitude);
+      u_y = 10 * (dir_y / magnitude);
+      new_i = i + u_x;
+      new_j = j + u_y;
+      line(i, j, new_i, new_j);
+    }
+  }
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
