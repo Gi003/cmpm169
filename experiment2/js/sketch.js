bdf0d4aeac97c76dc2ded523a2b43ec49d4a5edf
gi003 <diraheta@ucsc.edu>
2025-01-27 23:31:13 -0800

    changes

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -15,16 +15,10 @@ let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
+let grid = []; // Array to store grid squares
+let cols = 2;  // Initial number of columns
+let rows = 2;  // Initial number of rows
+let gridSize = 400; // Canvas/grid size in pixels
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -36,44 +30,28 @@ function resizeScreen() {
 
 // setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+  createCanvas(gridSize, gridSize);
+  grid = initializeGrid(0, 0, gridSize, gridSize, cols, rows);
+  print(grid);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  background(220);
+  for (let cell of grid) {
+    cell.show();
+  }
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  for (let index = grid.length - 1; index >= 0; index--){
+    cell = grid[index];
+    if (cell.contains(mouseX, mouseY)) {
+      addCell(cell,cols,rows);
+      print(grid);
+      break;
+    }
+  }
 }
